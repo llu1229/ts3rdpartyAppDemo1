9c2137e708e5c735e1d20b73009714fca8b0cb4f
LJ <lji@tradeshift.com>
2014-07-30 17:43:41 +0800

    clean up home page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 var config = require('../config.js');
 var util = require('util');
 /* GET home page. */
-router.get('/', function(req, res) {
-	var url = util.format('http://%s:%s%s/auth/login?response_type=code&client_id=%s&redirect_uri=http://%s:%s/service&scope=openid',
-  	  config.host, config.port, config.proxyRootPath, config.authId, config.thirdPartyHost, config.thirdPartyPort);
-  	console.log('redirect to '+ url);
-  res.redirect(url);
+//router.get('/', function(req, res) {
+//	var url = util.format('http://%s:%s%s/auth/login?response_type=code&client_id=%s&redirect_uri=http://%s:%s/service&scope=openid',
+//  	  config.host, config.port, config.proxyRootPath, config.authId, config.thirdPartyHost, config.thirdPartyPort);
+//  	console.log('redirect to '+ url);
+//  res.redirect(url);
+//});
+
+router.get('/', function(req, res){
+    res.sendfile(path.join(__dirname, 'public','index.html'));
 });
 
 module.exports = router;
