87c36c03d493fb2422140fdd788a1fe0d24f17d3
llu1229 <llu@tradeshift.com>
2014-08-04 14:12:17 +0800

    for dev demo app

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 config = {
-	proxyRootPath: '/tradeshift-proxy',
-	host: '172.16.33.210',
-	port: 8889,
-	authId: 'LewisCompany.DemoService3',
-	thirdPartyHost: '172.16.33.229',
-	thirdPartyPort: 3000
+    proxyRootPath: '/tradeshift-proxy',
+    host: 'localhost',
+    port: 8889,
+    authId: 'Tradeshift.DevDemoURL',
+    thirdPartyHost: 'localhost',
+    thirdPartyPort: 3000
 };
 
 module.exports = config;
